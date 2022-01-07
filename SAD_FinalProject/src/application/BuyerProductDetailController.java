ebd0854fc970964e8c0c31428f788a6de7d916c8
J-ihsuan <74034749+J-ihsuan@users.noreply.github.com>
2022-01-07 13:07:24 +0800

    0107update

--- SAD_FinalProject/src/application/BuyerProductDetailController.java
+++ SAD_FinalProject/src/application/BuyerProductDetailController.java
@@ -19,4 +19,87 @@ public class BuyerProductDetailController {
 		stage.setScene(scene);
 		stage.show();
 	}
+	@FXML private Label Dinfo;
+	@FXML private Label Ddescrip;
+	@FXML private Label Dplace;
+	@FXML private Label Dtype;
+	@FXML private Label Dend;
+	@FXML private Label Dtime;
+	@FXML private Label Dstart;
+	@FXML private Label Dnum;
+	@FXML private Label Dprice;
+	@FXML private Label Dtitle;
+	//@FXML private ImageView picture;
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1){
+		File file = new File("/Users/ihsuan/eclipse-workspace/0101sad/src/application/goodlist.txt");
+		Scanner scanner;
+		int i=1;
+		try {
+			scanner = new Scanner(file);
+			while(scanner.hasNext()&&i<11) {
+				String info=scanner.next();
+				switch(i) {
+				case 1:
+					Dtitle.setText(info);
+					i++;
+					break;
+				case 2:
+					Dprice.setText(info);
+					i++;
+					break;
+				case 3:
+					Dnum.setText(info);
+					i++;
+					break;
+				case 4:
+					Dtype.setText(info);
+					i++;
+					break;
+				case 5:
+					String timeString=info;
+					info=scanner.next();
+					timeString+=" "+info;
+					Dstart.setText(timeString);
+					i++;
+					break;
+				case 6:
+					String timeS=info;
+					info=scanner.next();
+					timeS+=" "+info;
+					Dend.setText(timeS);
+					i++;
+					break;
+				case 7:
+					Dtime.setText(info);
+					i++;
+					break;
+				case 8:
+					Dplace.setText(info);
+					i++;
+					break;
+				case 9:
+					Dinfo.setText(info);
+					i++;
+					break;
+				case 10:
+					Ddescrip.setText(info);
+					i++;
+					break;
+				//case 11:
+				//	String path="/Users/ihsuan/eclipse-workspace/0101sad/src/"+info;//C:\\Users\\Danny\\git\\sad\\SAD_FinalProject\\src\\
+					//FileInputStream pic=new FileInputStream(path);
+					//Image ig=new Image(pic);
+				//	picture.setImage(new Image(new FileInputStream(path)));
+				//	picture.setFitHeight(200);
+				//	picture.setFitWidth(200);
+				//	break;
+					}
+				}
+			scanner.close();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
 }
